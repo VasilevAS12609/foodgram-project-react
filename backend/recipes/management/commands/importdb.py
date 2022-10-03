02f3184c64c9309268a0c45998e17bec714e38c9
Алексей Васильев <VasilevAS12609@gmail.com>
2022-10-03 12:19:40 +0300

    First

--- backend/recipes/management/commands/importdb.py
+++ backend/recipes/management/commands/importdb.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 import json
 from recipes.models import Ingredient
-import os
 
 
 class Command(BaseCommand):
@@ -10,7 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         BASE_DIR = settings.BASE_DIR
-        file = os.path.dirname(BASE_DIR) + '\data\ingredients.json'
+        file = BASE_DIR + '\data\ingredients.json'
         with open(file, "r", encoding='utf-8') as read_file:
             data = json.load(read_file)
             for i in data:
