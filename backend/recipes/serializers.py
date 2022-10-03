18734fa829f354e39a445a7293ead6f8793168e4
Алексей Васильев <VasilevAS12609@gmail.com>
2022-10-03 14:42:42 +0300

    First

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,8 +1,7 @@
-import traceback
-
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from users.mixins import IsSubscribedMixin
+from django.db import IntegrityError
 
 from users.models import User
 
@@ -105,13 +104,12 @@ class RecipeSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         context = self.context['request']
         ingredients = validated_data.pop('recipe_ingredients')
-        if Recipe.objects.create(**validated_data,
-                                 author=self.context.get('request').user):
+        try:
             recipe = Recipe.objects.create(
                 **validated_data,
                 author=self.context.get('request').user
             )
-        else:
+        except IntegrityError as err:
             pass
         tags_set = context.data['tags']
         for tag in tags_set:
